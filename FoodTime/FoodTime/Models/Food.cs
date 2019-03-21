f91a4cffa47af3f0de9ce6336a4d5d30bac89f2c
Michael Benko <godyatochka@gmail.com>
2019-03-21 10:52:00 +0200

    add Food(Pizza, Sushi) and User models
    add Food(Pizza, Sushi) and User models

---
+++ FoodTime/FoodTime/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTime.Models
+{
+    public abstract class Food
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Componets { get; set; }
+        public int Weight { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ FoodTime/FoodTime/Models/Sushi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTime.Models
+{
+    public class Sushi: Food
+    {
+        public string SubCategory { get; set; }
+
+        public Sushi()
+        {
+            base.Name = "No data";
+            base.Componets = "No data";
+            base.Weight = 0;
+            base.Category = "Sushi";
+            base.Price = 0.0;
+            SubCategory = "No data";
+        }
+    }
+}
