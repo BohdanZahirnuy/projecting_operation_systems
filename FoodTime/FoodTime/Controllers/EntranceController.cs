2340096a011beeff6a1f81c3c0cb9e7a8eb1e576
Bohdan Zahirnuy <zahirnuy@ukr.net>
2019-03-22 16:11:53 +0200

    added Entrance controller and view

---
+++ FoodTime/FoodTime/Controllers/EntranceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTime.Controllers
+{
+    public class EntranceController : Controller
+    {
+        // initialize database, when it'll be ready
+        public IActionResult SignIn()
+        {
+           
+            return View();
+        }
+    }
+}
