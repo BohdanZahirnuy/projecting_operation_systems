af7199b642b8b5e8e0f25f7fa8df55abfa9713b3
zlatilda <43010223+zlatilda@users.noreply.github.com>
2019-03-28 19:07:40 +0200

    controller

--- FoodTime/FoodTime/Controllers/FoodController.cs
+++ FoodTime/FoodTime/Controllers/FoodController.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodTime.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace FoodTime.Controllers
 {
+    [Route("food")]
     public class FoodController : Controller
     {
+        private DataContext db = new DataContext();
+
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
         // initialize database, when it'll be ready
         public IActionResult FoodDetail()
         {
             //uncomment when db'll be ready
             //var food = db.Food.SingleOrDefault(x => x.Id == id);
             //return View(food);
+            ViewBag.foods = db.Food.ToList();
             return View();
         }
     }
