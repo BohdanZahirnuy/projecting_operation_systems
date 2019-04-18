77620fa65323a9ced56e8c771eea3bde230a8761
Michael Benko <godyatochka@gmail.com>
2019-04-19 01:12:43 +0300

    add home controller and configurate foodcontext

--- FoodTime/FoodTime.Data/FoodContext.cs
+++ FoodTime/FoodTime.Data/FoodContext.cs
@@ -2,19 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
 
 namespace FoodTime.Data
 {
     public class FoodContext : DbContext
     {
-        public FoodContext()
+        public FoodContext() 
         {
+            
 
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=aspnet-FoodTime-A008C036-0394-4387-8762-5247BC8B3DDE;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            }
+
         }
+           
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- FoodTime/FoodTime/Controllers/HomeController.cs
+++ FoodTime/FoodTime/Controllers/HomeController.cs
@@ -5,13 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FoodTime.Models;
+using FoodTime.Data;
+using FoodTime.Data.Models;
 
 namespace FoodTime.Controllers
 {
     public class HomeController : Controller
     {
+        FoodContext db = new FoodContext();
         public IActionResult Index()
         {
+            IEnumerable<Food> foods =  db.Food;
+            ViewBag.Foods = foods;
             return View();
         }
     }
