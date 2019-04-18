976cd8fc786938e6d80582a10d1fca03e0de5a44
Bohdan Zahirnuy <zahirnuy@ukr.net>
2019-04-19 00:07:49 +0300

    added FoodContext

---
+++ FoodTime/FoodTime.Data/FoodContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodTime.Data
+{
+    public class FoodContext : DbContext
+    {
+        public FoodContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //entities
+        public DbSet<FoodTime.Data.Models.Food> Food { get; set; }
+
+    }
+}
